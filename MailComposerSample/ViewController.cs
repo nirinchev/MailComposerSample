61756954c14cf0fc9ce35faa9936bd1f899ef7f2
Nikola Irinchev <irinchev@me.com>
2015-08-25 01:45:20 +0300

    Initial commit

---
+++ MailComposerSample/ViewController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using UIKit;
+using MessageUI;
+
+namespace MailComposerSample
+{
+	public partial class ViewController : UIViewController
+	{
+		public ViewController(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override void ViewDidAppear(bool animated)
+		{
+			base.ViewDidAppear(animated);
+
+			var mailController = new MFMailComposeViewController();
+			mailController.SetToRecipients(new[] { "me@example.com" });
+			mailController.SetSubject("This seems to work :)");
+			mailController.SetMessageBody("Lorem ipsum", false);
+			mailController.Finished += (sender, e) => 
+			{
+				this.DismissViewController (true, null);
+			};
+			this.PresentViewController(mailController, true, null);
+		}
+	}
+}
